25682e414970b1ff30e5a09502c82bf668612c6d
RK <rumaisa.kashif2109@gmail.com>
2025-05-14 17:39:11 +0500

    Added tabs

--- myOvaiApp/app/(tabs)/symptom-logger.tsx
+++ myOvaiApp/app/(tabs)/symptom-logger.tsx
@@ -1,17 +1,11 @@
-import { Image } from 'expo-image';
 import { View, Text, Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+export default function TabThreeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <SafeAreaView style={styles.titleContainer}>
-        <Text style={styles.text}>Welcome to myOvai!</Text>
+        <Text style={styles.text}>Symptom Logger</Text>
       </SafeAreaView>
     </SafeAreaView>
   );
@@ -27,7 +21,7 @@ const styles = StyleSheet.create({
   text: {
     marginTop: 40,
     alignItems: 'center',//horizontally centre
-    justifyContent: 'centre',//vertically centre
+    justifyContent: 'center',//vertically centre
     color: 'white',
     fontSize: 30,
     fontWeight: "bold",
