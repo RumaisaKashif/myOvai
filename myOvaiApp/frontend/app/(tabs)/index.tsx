a75f244dbc28079fa2f9ad8dcb189748cb2db040
RK <rumaisa.kashif2109@gmail.com>
2025-05-15 20:47:25 +0500

    Added authentication, logout doesn't work yet

--- myOvaiApp/frontend/app/(tabs)/index.tsx
+++ myOvaiApp/frontend/app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text, Platform, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
 
 export default function HomeScreen() {
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      console.log('User signed out successfully');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <SafeAreaView style={styles.titleContainer}>
         <Text style={styles.text}>Welcome to myOvai!</Text>
       </SafeAreaView>
+      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+        <Text style={styles.logoutText}>Logout</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -39,5 +53,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#602495",
     borderWidth: 1, // Border thickness
     borderBottomColor: "white", // Border color
+  },
+  logoutButton: {
+    backgroundColor: "#602495",
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 20,
+    width: '10%',
+    alignItems: 'center',
+  },
+  logoutText: { 
+    color: 'white',
+    fontSize: 20,
+    fontFamily: "Helvetica",
+    padding: 10,
+    alignItems: "center"
   }
 });
