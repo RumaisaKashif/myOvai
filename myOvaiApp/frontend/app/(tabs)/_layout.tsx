f6c46af44f63a4dbd63ba139b0703b9011e83490
Rumaisa Kashif <142867414+RumaisaKashif@users.noreply.github.com>
2025-05-16 13:55:17 +0500

    Update _layout.tsx

--- myOvaiApp/frontend/app/(tabs)/_layout.tsx
+++ myOvaiApp/frontend/app/(tabs)/_layout.tsx
@@ -2,11 +2,6 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Ionicons } from "@expo/vector-icons";
 import { Platform } from 'react-native';
-
-// import { HapticTab } from '@/components/HapticTab';
-// import { IconSymbol } from '@/components/ui/IconSymbol';
-// import TabBarBackground from '@/components/ui/TabBarBackground';
-// import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
@@ -44,17 +39,3 @@ export default function TabLayout() {
     </Tabs>
   );
 }
-
-// screenOptions={{
-      //   tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      //   headerShown: false,
-      //   tabBarButton: HapticTab,
-      //   tabBarBackground: TabBarBackground,
-      //   tabBarStyle: Platform.select({
-      //     ios: {
-      //       // Use a transparent background on iOS to show the blur effect
-      //       position: 'absolute',
-      //     },
-      //     default: {},
-      //   }),
-      // }}>
